e727b028c81f8fa95ac9156ca2b68543778190ae
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 09:56:58 -0600

    Fixed the loading of fullpost.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -21,24 +21,31 @@ class FullPost extends Component {
         }      
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts' + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
         if(this.props.id){
+
             post = <p style={{textAlign: 'center'}}>Loading...</p>;
         }
-
-            if (this.props.loadedPost){
-            post = (
-                <div className="FullPost">
-                    <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.body}</p>
-                    <div className="Edit">
-                        <button className="Delete">Delete</button>
-                    </div>
-                </div>      
-            );
-         }
+            if (this.state.loadedPost){
+                post = (
+                    <div className="FullPost">
+                        <h1>{this.state.loadedPost.title}</h1>
+                        <p>{this.state.loadedPost.body}</p>
+                        <div className="Edit">
+                            <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                        </div>
+                    </div>      
+                );
+            }
         return post;
     }
 }
